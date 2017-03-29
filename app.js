5987ee4a75a24eb646b4ef72c2d6e616cf17850c
lecosas <lecosas@gmail.com>
2017-03-29 09:57:52 -0300

    adicionada a variável outraVariavel

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ var bodyParser = require('body-parser');
 var index = require('./routes/index');
 var users = require('./routes/users');
 
+var outraVariavel = "aqui aqui";
+
 var app = express();
 
 // view engine setup
